3be5e4e643ccf890eb9c867bb56d3d53e41c3453
Sid <mogyahid.projects@protonmail.com>
2023-09-01 20:38:14 +0800

    Transferred layout from blade to livewire

---
+++ resources/views/livewire/calendar/index.blade.php
@@ -0,0 +1,5 @@
+<div>
+    <div class="py-6 text-gray-900">
+        <h1 class="font-semibold text-lg">Calendar</h1>
+    </div>
+</div>
---
+++ resources/views/livewire/planning/index.blade.php
@@ -0,0 +1,5 @@
+<div>
+    <div class="py-6 text-gray-900">
+        <h1 class="font-semibold text-lg">Planning</h1>
+    </div>
+</div>
