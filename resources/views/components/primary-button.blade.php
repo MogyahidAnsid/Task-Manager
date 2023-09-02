e2866ec98f520450f01a83394a3edef8445fb16c
Sid <mogyahid.projects@protonmail.com>
2023-09-02 23:45:52 +0800

    Changed primary-button padding

--- resources/views/components/primary-button.blade.php
+++ resources/views/components/primary-button.blade.php
@@ -1,6 +1,6 @@
 @props([ 'type' => 'submit'])
 
-<button type="{{ $type }}" {{ $attributes->merge(['class' => 'inline-flex items-center justify-center px-4 rounded-md py-2 bg-orange-500 border border-transparent font-medium text-sm text-white hover:bg-orange-400 focus:bg-orange-500
+<button type="{{ $type }}" {{ $attributes->merge(['class' => 'inline-flex items-center justify-center p-2 rounded-md bg-orange-500 border border-transparent font-medium text-sm text-white hover:bg-orange-400 focus:bg-orange-500
     active:bg-orange-600 focus:outline-none focus:ring-2 focus:ring-orange-500 focus:ring-offset-2 transition ease-in-out duration-150']) }}>
     {{ $slot }}
 </button>
