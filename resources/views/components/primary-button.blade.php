880ce826d14b47ba2f33a0550ac2713df4416f96
Sid <mogyahid.projects@protonmail.com>
2023-09-02 23:35:57 +0800

    Modified primary-button component padding-y

--- resources/views/components/primary-button.blade.php
+++ resources/views/components/primary-button.blade.php
@@ -1,4 +1,4 @@
-<button {{ $attributes->merge(['type' => 'submit', 'class' => 'inline-flex items-center justify-center px-4 rounded-md py-3 bg-orange-500 border border-transparent font-semibold text-xs text-white uppercase tracking-widest hover:bg-orange-400 focus:bg-orange-500
+<button {{ $attributes->merge(['type' => 'submit', 'class' => 'inline-flex items-center justify-center px-4 rounded-md py-2 bg-orange-500 border border-transparent font-semibold text-xs text-white uppercase tracking-widest hover:bg-orange-400 focus:bg-orange-500
     active:bg-orange-600 focus:outline-none focus:ring-2 focus:ring-orange-500 focus:ring-offset-2 transition ease-in-out duration-150']) }}>
     {{ $slot }}
 </button>
